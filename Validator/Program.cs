90bc5a73d044755b965c22680b17858ba0ea151f
Robert Bandstra <rob.bandstra@gmail.com>
2021-01-15 20:31:21 -0600

    user can enter pin and see if it is valid or not

---
+++ Validator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a PIN code between 4 and 8 digits inclusive");
+            string pinCode = Console.ReadLine();
+            bool onlyDigits = true;
+
+            while (true)
+            {
+                foreach (var digit in pinCode)
+                    if (Char.IsDigit(digit))
+                    {
+                        onlyDigits = true;
+                    }
+                    else
+                    {
+                        onlyDigits = false;
+                        break;
+                    }
+                break;
+            }
+            bool validPin = pinCode.Length >= 4 && pinCode.Length <= 8 && onlyDigits == true;
+
+
+            Console.WriteLine(validPin);
+        }
+    }
+}
